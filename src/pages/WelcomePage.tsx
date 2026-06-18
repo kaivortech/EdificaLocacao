fabb92b5b58477dcf245b99062d8cfdeeafabb7e
Johnny <johnnypereiradacostajr@gmail.com>
2026-06-18 16:57:34 -0300

    Limitar campo CPF a 11 dígitos

--- src/pages/WelcomePage.tsx
+++ src/pages/WelcomePage.tsx
@@ -25,12 +25,11 @@ const WelcomePage: React.FC = () => {
   const [regLoading, setRegLoading] = useState(false);
 
   const formatCPF = (value: string): string => {
-    const digits = value.replace(/\D/g, '');
+    const digits = value.replace(/\D/g, '').slice(0, 11);
     return digits
       .replace(/(\d{3})(\d)/, '$1.$2')
       .replace(/(\d{3})(\d)/, '$1.$2')
-      .replace(/(\d{3})(\d{1,2})$/, '$1-$2')
-      .slice(0, 14);
+      .replace(/(\d{3})(\d{1,2})$/, '$1-$2');
   };
 
   const validateCPF = (cpf: string): boolean => {
